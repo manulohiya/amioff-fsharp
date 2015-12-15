63242decefbd3dd87af58d172747a9325f52d3ba
Manu Lohiya <manulohiya@gmail.com>
2015-12-14 22:42:20 -0800

    Program name input

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ $(document).ready()
 
   console.log("JS is ready!")
 
-  $('#datetimepicker').datetimepicker({
-        format: 'MM/dd/yyyy hh:mm',
-        language: 'en',
-        pickSeconds: false, 
-        pick12HourFormat: true
-      });
 
+$("#program-search").submit(function(event) {
+	console.log("Submit button is working")
+	event.preventDefault();
+	var $programName = $('#program-name').val();
+	console.log("Program Name: "+$programName)
+	
+});
+
+
+
+
+$('#datetimepicker').datetimepicker({
+    format: 'MM/dd/yyyy hh:mm',
+    language: 'en',
+    pickSeconds: false, 
+    pick12HourFormat: true
+});
+
+
+
+
+// "Chang, Julia",2529,922,"UCSF-EM 3p-11p (Blue)",429,124,12-1-15,1500,2300
