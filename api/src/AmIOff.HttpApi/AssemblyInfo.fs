3f51e21e0df5c11f2d20c6a79b6299feb470d498
Neil Maheshwari <neil@beyongames.co>
2015-12-19 14:14:41 -0800

    Initial commit for api directory using FSharp.ProjectScaffold

---
+++ api/src/AmIOff.HttpApi/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("AmIOff.HttpApi")>]
+[<assembly: AssemblyProductAttribute("AmIOff.HttpApi")>]
+[<assembly: AssemblyDescriptionAttribute("Http api for medical residents to find fellow residents to hang out with.")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ api/tests/AmIOff.HttpApi.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module AmIOff.HttpApi.Tests
+
+open AmIOff.HttpApi
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
