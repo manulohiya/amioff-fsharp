b9d651a940fd8543a4edc57c9b3f99e58d70f7da
Neil Maheshwari <neil@beyongames.co>
2015-12-19 14:30:42 -0800

    Add reference to FSharp.Data in AmIOff.Http/Script.fsx.

--- api/src/AmIOff.HttpApi/Script.fsx
+++ api/src/AmIOff.HttpApi/Script.fsx
@@ -2,6 +2,9 @@
 // for more guidance on F# programming.
 
 #load "Library.fs"
+#I @"../../packages/FSharp.Data/lib/portable-net40+sl5+wp8+win8/"
+#r "FSharp.Data.dll"
+
 open AmIOff.HttpApi
 
 let num = Library.hello 42
