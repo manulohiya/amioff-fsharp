e113902c3c3bc90a4ba8d8404d6f60d55cbe25b1
Manu Lohiya <manulohiya@gmail.com>
2015-12-19 22:50:31 -0800

    templates

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
-$(document).ready()
+$(function(){
 
   console.log("JS is ready!")
 
 
+// results template
+var $results = $('#results');
+var _results = _.template($('#results-template').html());
+
+
+// On page load
+
+
+
+// On submit button
 $("#program-search").submit(function(event) {
 	console.log("Submit button is working")
 	event.preventDefault();
+	$results.empty();
 	var $programName = $('#program-name').val();
+	var $date = $('#date').val();
 	console.log("Program Name: "+$programName)
+	console.log("Date: "+$date)
 	
+	// $.get('url+$programName+ $date',
+	   var names = [
+  {firstName: "Manu", lastName: "Lohiya"},
+  {firstName: "Neil", lastName: "Maheshwari"}
+
+];
+	   // var names = 
+	   // {
+    //     firstName: "Manu",
+    //     lastName: "Lohiya"
+    // 	};
+
+    // 	var name2 = 	
+    // 	{
+    // 	firstName: "Neil",
+    //     lastName: "Maheshwari"
+    // 	};
+    
+    // 	var data = [name1,name2];
+    	console.log(names)	
+		
+    	_.each(names, function (name, index) {
+  		var $name = $(_results(name));
+  		$name.attr('data-index', index);
+  		$results.append($name);
+		});
+
+		
 });
 
 
@@ -20,6 +61,9 @@ $('#datetimepicker').datetimepicker({
     pickSeconds: false, 
     pick12HourFormat: true
 });
+
+});
+
 
 
 
