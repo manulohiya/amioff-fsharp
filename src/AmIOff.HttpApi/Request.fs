22a6546e0c6aff14b35cf4cfd387e39f76c7cd4f
Neil Maheshwari <neil@beyongames.co>
2015-12-28 19:04:43 +0530

    Removing print statements from resident is busy.

--- src/AmIOff.HttpApi/Request.fs
+++ src/AmIOff.HttpApi/Request.fs
@@ -264,12 +264,6 @@ module Timesheet =
             scheduleItem 
             |> maybeStartTime offset
             |> Option.exists (fun startTime ->
-                let id = hash scheduleItem
-                printfn "RESIDENT: %s %s" resident.first resident.last
-                printfn "%s %d StartTime: %A" resident.first id startTime
-                printfn "%s %d Input Time: %A" resident.first id time 
-                printfn "%s %d Input TimeKind: %A" resident.first id time.Kind
-                printfn "%s %d StartTimeKind: %A" resident.first id startTime.Kind
                 let isAfterStart = startTime <= time
                 let isBeforeEnd = 
                     let maybeEndTime = 
@@ -277,12 +271,8 @@ module Timesheet =
                         |> maybeEndTime offset startTime
                     match maybeEndTime with
                     | Some endTime -> 
-                        printfn "%s %d EndTimeKind: %A" resident.first id endTime.Kind
-                        printfn "%s %d EndTime: %A" resident.first id endTime
                         time <= endTime
                     | None -> false
-                printfn "%s %d IsAfterStart: %A" resident.first id isAfterStart
-                printfn "%s %d isBeforEnd: %A" resident.first id isBeforeEnd
                 isAfterStart && isBeforeEnd))
 
     let freeResidents residents time offset timesheet = 
