ad7834a0abec7d4640f53801346e563af40a7b7d
Neil Maheshwari <neil@beyongames.co>
2016-02-06 18:39:18 -0800

    Adding log4net to suave.

--- src/AmIOff.HttpApi/Script.fsx
+++ src/AmIOff.HttpApi/Script.fsx
@@ -7,6 +7,8 @@ printfn "OK"
 #r "Suave.dll"
 #I @"../../packages/FSharp.Data/lib/net40/"
 #r "FSharp.Data.dll"
+#I @"../../packages/log4net/lib/net45-full/"
+#r "log4net.dll"
 
 #I "bin/Debug"
 #r "AmIOff.HttpApi.exe"
@@ -20,11 +22,38 @@ open Suave.Http.Successful
 open Suave.Web
 open Suave.Json
 open Suave.Types
+open log4net
+
+type Log4Net () = 
+    let fileInfo = new System.IO.FileInfo (__SOURCE_DIRECTORY__ + "/app.config")
+    let _ = log4net.Config.XmlConfigurator.Configure (fileInfo)
+    let logger = LogManager.GetLogger "Suave" 
+    interface Logging.Logger with
+        member x.Log level fn = 
+            match level with
+            | Logging.LogLevel.Debug
+            | Logging.LogLevel.Verbose ->
+                let msg = (fn ()).message
+                logger.Debug msg
+            | Logging.LogLevel.Error ->
+                let msg = (fn ()).message
+                logger.Warn msg
+            | Logging.LogLevel.Warn -> 
+                let msg = (fn ()).message
+                logger.Debug msg
+            | Logging.LogLevel.Fatal ->
+                let msg = (fn ()).message
+                logger.Debug msg
+            | Logging.LogLevel.Info ->
+                let msg = (fn ()).message
+                logger.Debug msg
+
+
 
 let main args =
     printfn "Arguments passed to function : %A" args
-    // Return 0. This indicates success.
-
+    let logger = LogManager.GetLogger "main"
+    let log4net = new Log4Net ()
     let serverConfig = 
       let port = 
         try
@@ -33,7 +62,7 @@ let main args =
             | exn -> 3000
       { Web.defaultConfig with
           homeFolder = Some __SOURCE_DIRECTORY__ 
-          logger = Logging.Loggers.saneDefaultsFor Logging.LogLevel.Verbose
+          logger = log4net
           bindings = [ Types.HttpBinding.mk' Types.HTTP "0.0.0.0" port ] }
 
     Web.startWebServer serverConfig Service.residentsApp
