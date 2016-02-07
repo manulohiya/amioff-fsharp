45afa80a83f9a28855d72190cbd94f4ad9844b86
Neil Maheshwari <neil@beyongames.co>
2016-02-06 22:11:07 -0800

    hangStub working. swapStub broken.

--- src/AmIOff.HttpApi/WebService.fs
+++ src/AmIOff.HttpApi/WebService.fs
@@ -8,6 +8,15 @@ open Suave.Web
 open Suave.Json
 open Suave.Types
 open Suave.Http.Files
+open System.Collections.Generic
+open Newtonsoft.Json 
+open System
+
+type StaffObject = {id : int; firstName : string; lastName : string; staffType : string}
+type HangResponseObject = {staffObject : StaffObject; grouping : string; freeUntil : int64}
+type HangResponse = {programName : string; inputTime : int64; staff : List<HangResponseObject>}
+type SwapResponseObject = {staffObject : StaffObject}
+type SwapResponse = {programName : string; start : int64; endTime : int64; swapWindow : int64; staff : List<SwapResponseObject>} 
 
 module Service = 
 
@@ -46,9 +55,44 @@ module Service =
                 | None -> return! Suave.Http.RequestErrors.BAD_REQUEST "Invalid login or date" x
         }                
 
+    let (^^) (r : HttpRequest) (key : string) = 
+        match r.queryParam key with
+        | Choice1Of2 x -> Some x
+        | _ -> 
+            logger.Warn <| sprintf "Could not find query param %s" key
+            None 
+
+    let (^^!) r k = Option.get (r ^^ k)
+
+    let hangStub (r : HttpRequest) = 
+        logger.Info <| sprintf "Hang Query: %s" r.rawQuery
+        let staffType = r ^^ "staffType" |> Option.fold (fun _ s -> s) "UCSFEM-R1"
+        let grouping = r ^^ "grouping" |> Option.fold (fun _ s -> s) "Vacation"
+        let delphine = {id = 1; firstName = "Delphine"; lastName = "Huang"; staffType =  staffType}
+        let o = {staffObject = delphine; grouping = grouping; freeUntil = 1L}
+        let xs = new List<_>()
+        xs.Add(o)
+        {programName = (r ^^! ("programName")); inputTime = int64(r ^^! "time"); staff = xs}
+        |> JsonConvert.SerializeObject
+
+    let swapStub (r : HttpRequest) = 
+        logger.Info <| sprintf "Swap Query: %s" r.rawQuery
+        let staffType = r ^^ "staffType" |> Option.fold (fun _ s -> s) "UCSFEM-R1"
+        let delphine = {id = 1; firstName = "Delphine"; lastName = "Huang"; staffType = staffType} 
+        let programName = r ^^! "programName"
+        let start = int64(r ^^! "startTime")
+        let endTime = int64(r ^^! "endTime")
+        let (o : SwapResponseObject) = {staffObject = delphine}
+        let xs = new List<_>()
+        xs.Add(o)
+        {programName = programName; start = start; endTime = endTime; staff = xs; swapWindow = 0L}
+        |> JsonConvert.SerializeObject
+
     let routes = 
         let returnJson = Writers.setMimeType "application/json; charset=utf-8"
         [
+          path "/api/hang" >>= request (OK << hangStub) >>= returnJson
+          path "/api/swap" >>= request (OK << swapStub) >>= returnJson
           pathScan "/api/%s/%d" findFreeResidentsAsJson >>= returnJson
           path "/" >>= Suave.Http.Files.file "content/index.html"
           pathScan "/%s" (Suave.Http.Files.file  << sprintf "content/%s")
