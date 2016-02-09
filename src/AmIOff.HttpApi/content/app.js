be9129bd74c06258b347749d36142730b3ee7714
Manu Lohiya <manulohiya@gmail.com>
2016-02-08 22:12:46 -0800

    GET request from API

--- src/AmIOff.HttpApi/content/app.js
+++ src/AmIOff.HttpApi/content/app.js
@@ -2,19 +2,11 @@ $(function(){
 
 	console.log("JS is ready!")
 
-
 // results templates
 var $resultsOn = $('#results-on');
 var _resultsOn = _.template($('#resultsOn-template').html());
 
-
-
-
-
-
-
-
-// On page load
+// On page load, set up date and time pickers
 $("#datepicker" ).datepicker({ 
 	minDate: 0, 
 	maxDate: 30 
@@ -26,8 +18,9 @@ $('#timepicker').timepicker({
 
 });
 
-$('#heading-on').hide();
-$('#heading-off').hide();
+	// Hide headings under results 
+	$('#heading-on').hide();
+	$('#heading-off').hide();
 
   // Retrieve the users program-name
   var name = localStorage.getItem('program-name');
@@ -36,14 +29,15 @@ $('#heading-off').hide();
   	$("#program-name").val(name);
   } else {
 
-  	$("#program-name").val("e.g. UCSFEMx`");
+  	$("#program-name").val("e.g. UCSFEM");
   }
 
-
+  // Function to check if element is present in an array
   function isInArray(value, array) {
   	return array.indexOf(value) > -1;
   }
 
+  // Function to convert date from string to numerical values
   var dateChecker = function(date) {
   	console.log("dateChecker function is working")
   	var newDate = date;
@@ -54,7 +48,7 @@ $('#heading-off').hide();
   	var month = parseInt(myArray);
   	var year = parseInt(myArray2);
 
-
+  	// Convert calendar year to academic year
   	if (month <= 6) {
   		console.log("Month is between 1 and 6");	
   		year = year - 1;
@@ -66,24 +60,13 @@ $('#heading-off').hide();
   	return newDate
   };
 
-// var date = new Date(unixTimestamp*1000);
-
-// console.log("UnixTime now: "+timeInMs)
-// console.log("Time now: "+date)
-
 
 // On submit button
 $("#program-search").submit(function(event) {
 	console.log("Submit button is working")
-	// $heading.empty();
-	
-	
-	
-	// event.preventDefault();
-	
-	
+	event.preventDefault();
 
-
+// Get values from form
 	var $programName = $('#program-name').val();
 	var $calendarDate = $('#datepicker').val();
 	var $date = dateChecker($calendarDate);
@@ -91,17 +74,13 @@ $("#program-search").submit(function(event) {
 	var $timeZone = $('#timeZone :selected').text();
 	var dateTimeZone = $date+" "+$time+" "+$timeZone;
 
+// Create time object 
 	var timeObject = {time : $time, calendarDate : $calendarDate, timeZone : $timeZone, programName: $programName }
-	var $inputTime = $(_heading(timeObject));
-	 // $inputTime.attr('data-index', index);
-	 $heading.append($inputTime);
 
-	// var value = $('#dropDownId:selected').text()
-	// var currentTime = $('#date').val();
 	
 	console.log("Program Name: "+$programName)
 	console.log("Date with timezone: "+dateTimeZone)
-
+// Save program name in local storage
 	localStorage.setItem("program-name", $programName);
 	var storedValue = localStorage.getItem("program-name");
 	console.log(storedValue);
@@ -111,6 +90,7 @@ $("#program-search").submit(function(event) {
 
 	console.log("Unix-timezone being sent to server: "+unixtime)
 
+// Send GET request to API
 	$.ajax({
         // url: "https://amioff.work/api/hang?programName=$programName%20time:unixtime",
         url: "/api/hang?programName="+$programName+"&"+"time="+unixtime,
@@ -126,6 +106,7 @@ $("#program-search").submit(function(event) {
             	var date = moment.unix(resident.freeUntil).format("M/D  (ddd)")
             	var freeUntil = moment.unix(resident.freeUntil).format("ha")
             	var grouping = resident.grouping;
+            	console.log("grouping: "+grouping);
             	var staffType = resident.staffObject.staffType;
             	var residentObject = {firstName : firstName, lastName : lastName, date : date, freeUntil : freeUntil, staffType: staffType, grouping: grouping}
             	console.log("Resident Object: ",residentObject)
